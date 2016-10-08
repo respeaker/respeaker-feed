257cb1e0e7bea7b9414b0e3c764964e2d10fb910
Baozhu Zuo <zuobaozhu@gmail.com>
2016-10-08 02:13:00 -0700

    iwpriv ubus interface works now

--- mtk-sdk-wifi/src/apClient.c
+++ mtk-sdk-wifi/src/apClient.c
@@ -40,8 +40,185 @@
 #include "libubus.h"
 
 static struct ubus_context *ctx;
-static struct ubus_subscriber apClient_event;
-static struct blob_buf b;
+static struct blob_buf buf;
+
+struct survey_table
+{
+	char channel[4];
+	char ssid[33];
+	char bssid[20];
+	char security[23];
+	char *crypto;
+    char siganl[8];
+};
+
+static struct survey_table st[64];
+static int survey_count = 0;
+
+#define RTPRIV_IOCTL_SET (SIOCIWFIRSTPRIV + 0x02)
+static void iwpriv(const char *name, const char *key, const char *val)
+{
+	int socket_id;
+	struct iwreq wrq;
+	char data[64];
+
+	snprintf(data, 64, "%s=%s", key, val);
+	socket_id = socket(AF_INET, SOCK_DGRAM, 0);
+	strcpy(wrq.ifr_ifrn.ifrn_name, name);
+	wrq.u.data.length = strlen(data);
+	wrq.u.data.pointer = data;
+	wrq.u.data.flags = 0;
+	ioctl(socket_id, RTPRIV_IOCTL_SET, &wrq);
+	close(socket_id);
+}
+
+static void next_field(char **line, char *output, int n) {
+	char *l = *line;
+	int i;
+
+	memcpy(output, *line, n);
+	*line = &l[n];
+
+	for (i = n - 1; i > 0; i--) {
+		if (output[i] != ' ')
+			break;
+		output[i] = '\0';
+	}
+}
+
+#define RTPRIV_IOCTL_GSITESURVEY (SIOCIWFIRSTPRIV + 0x0D)
+static void wifi_site_survey(const char *ifname, const char* essid, int print)
+{
+	char *s = malloc(IW_SCAN_MAX_DATA);
+	int ret;
+	int socket_id;
+	struct iwreq wrq;
+	char *line, *start;
+
+	iwpriv(ifname, "SiteSurvey", (essid ? essid : ""));
+	sleep(5);
+	memset(s, 0x00, IW_SCAN_MAX_DATA);
+	strcpy(wrq.ifr_name, ifname);
+	wrq.u.data.length = IW_SCAN_MAX_DATA;
+	wrq.u.data.pointer = s;
+	wrq.u.data.flags = 0;
+	socket_id = socket(AF_INET, SOCK_DGRAM, 0);
+	ret = ioctl(socket_id, RTPRIV_IOCTL_GSITESURVEY, &wrq);
+	close(socket_id);
+	if (ret != 0)
+		goto out;
+
+	if (wrq.u.data.length < 1)
+		goto out;
+
+	/* ioctl result starts with a newline, for some reason */
+	start = s;
+	while (*start == '\n')
+		start++;
+
+	line = strtok((char *)start, "\n");
+	line = strtok(NULL, "\n");
+	survey_count = 0;
+	while(line && (survey_count < 64)) {
+		next_field(&line, st[survey_count].channel, sizeof(st->channel));
+		next_field(&line, st[survey_count].ssid, sizeof(st->ssid));
+		next_field(&line, st[survey_count].bssid, sizeof(st->bssid));
+		next_field(&line, st[survey_count].security, sizeof(st->security));
+        next_field(&line, st[survey_count].siganl, sizeof(st->siganl)); 
+		line = strtok(NULL, "\n");
+		st[survey_count].crypto = strstr(st[survey_count].security, "/");
+		if (st[survey_count].crypto) {
+			*st[survey_count].crypto = '\0';
+			st[survey_count].crypto++;
+			syslog(LOG_INFO, "Found network - %s %s %s %s %s\n",
+				st[survey_count].channel, st[survey_count].ssid, st[survey_count].bssid, st[survey_count].security,st[survey_count].siganl);
+		} else {
+			st[survey_count].crypto = "";
+		}
+		survey_count++;
+	}
+
+	if (survey_count == 0 && !print)
+		syslog(LOG_INFO, "No results");
+out:
+	free(s);
+}
+
+#if 1
+static struct survey_table* wifi_find_ap(const char *name)
+{
+	int i;
+
+	for (i = 0; i < survey_count; i++)
+		if (!strcmp(name, (char*)st[i].ssid))
+			return &st[i];
+
+	return 0;
+}
+
+
+#define lengthof(x) (sizeof(x) / sizeof(x[0]))
+
+/* This function is heavily similar to the wifi_repeater_start in
+ * net/wifi_core.c from microd (but changed to call ifdown/ifup instead
+ * of fiddling with interface configuration manually. */
+static void wifi_repeater_start(const char *ifname, const char *staname, const char *channel, const char *ssid,
+				const char *key, const char *enc, const char *crypto)
+{
+	char buf[100];
+	int enctype = 0;
+
+	iwpriv(ifname, "Channel", channel);
+	iwpriv(staname, "ApCliEnable", "0");
+	if ((strstr(enc, "WPA2PSK") || strstr(enc, "WPAPSKWPA2PSK")) && key) {
+		enctype = 1;
+		iwpriv(staname, "ApCliAuthMode", "WPA2PSK");
+	} else if (strstr(enc, "WPAPSK") && key) {
+		enctype = 1;
+		iwpriv(staname, "ApCliAuthMode", "WPAPSK");
+	} else if (strstr(enc, "WEP") && key) {
+		iwpriv(staname, "ApCliAuthMode", "AUTOWEP");
+		iwpriv(staname, "ApCliEncrypType", "WEP");
+		iwpriv(staname, "ApCliDefaultKeyID", "1");
+		iwpriv(staname, "ApCliKey1", key);
+		iwpriv(staname, "ApCliSsid", ssid);
+	} else if (!key || key[0] == '\0') {
+		iwpriv(staname, "ApCliAuthMode", "NONE");
+		iwpriv(staname, "ApCliSsid", ssid);
+	} else {
+		return;
+	}
+
+	if (enctype) {
+		if (strstr(crypto, "AES") || strstr(crypto, "TKIPAES"))
+			iwpriv(staname, "ApCliEncrypType", "AES");
+		else
+			iwpriv(staname, "ApCliEncrypType", "TKIP");
+		iwpriv(staname, "ApCliSsid", ssid);
+		iwpriv(staname, "ApCliWPAPSK", key);
+	}
+	iwpriv(staname, "ApCliEnable", "1");
+	snprintf(buf, lengthof(buf) - 1, "ifconfig '%s' up", staname);
+	system(buf);
+}
+#endif
+
+int check_assoc(const char *ifname)
+{
+	int socket_id, i;
+	struct iwreq wrq;
+
+	socket_id = socket(AF_INET, SOCK_DGRAM, 0);
+	strcpy(wrq.ifr_ifrn.ifrn_name, ifname);
+	ioctl(socket_id, SIOCGIWAP, &wrq);
+	close(socket_id);
+
+	for (i = 0; i < 6; i++)
+		if(wrq.u.ap_addr.sa_data[i])
+			return 1;
+	return 0;
+}
+
 
 enum {
 	SCAN_DEVICE,
@@ -52,75 +229,48 @@ static const struct blobmsg_policy scan_policy[] = {
 	[SCAN_DEVICE] = { .name = "device", .type = BLOBMSG_TYPE_STRING },
 };
 
-struct scan_request {
-	struct ubus_request_data req;
-	struct uloop_timeout timeout;
-	int fd;
-	int idx;
-	char data[];
-};
-
-static void apClient_scan_fd_reply(struct uloop_timeout *t)
-{
-	struct scan_request *req = container_of(t, struct scan_request, timeout);
-	char *data;
-
-	data = alloca(strlen(req->data) + 32);
-	sprintf(data, "msg%d: %s\n", ++req->idx, req->data);
-	if (write(req->fd, data, strlen(data)) < 0) {
-		close(req->fd);
-		free(req);
-		return;
-	}
-
-	uloop_timeout_set(&req->timeout, 1000);
-}
-
-static void apClient_scan_reply(struct uloop_timeout *t)
-{
-	struct scan_request *req = container_of(t, struct scan_request, timeout);
-	int fds[2];
-
-	blob_buf_init(&b, 0);
-	blobmsg_add_string(&b, "message", req->data);
-	ubus_send_reply(ctx, &req->req, b.head);
-
-	if (pipe(fds) == -1) {
-		fprintf(stderr, "Failed to create pipe\n");
-		return;
-	}
-	ubus_request_set_fd(ctx, &req->req, fds[0]);
-	ubus_complete_deferred_request(ctx, &req->req, 0);
-	req->fd = fds[1];
-
-	req->timeout.cb = apClient_scan_fd_reply;
-	apClient_scan_fd_reply(t);
-}
 
 static int apClient_scan(struct ubus_context *ctx, struct ubus_object *obj,
 		      struct ubus_request_data *req, const char *method,
 		      struct blob_attr *msg)
 {
-	struct scan_request *hreq;
+	//struct scan_request *hreq;
 	struct blob_attr *tb[__SCAN_MAX];
-	const char *format = "%s received a message: %s";
-	const char *msgstr = "(unknown)";
+	const char *msgstr;
+    void *c, *d;
+    int i;
 
-	blobmsg_parse(scan_policy, ARRAY_SIZE(scan_policy), tb, blob_data(msg), blob_len(msg));
+	blobmsg_parse(scan_policy,__SCAN_MAX , tb, blob_data(msg), blob_len(msg));
 
-	if (tb[SCAN_MSG])
-		msgstr = blobmsg_data(tb[SCAN_MSG]);
+    if (!tb[SCAN_DEVICE])
+		return UBUS_STATUS_INVALID_ARGUMENT;
 
-	hreq = calloc(1, sizeof(*hreq) + strlen(format) + strlen(obj->name) + strlen(msgstr) + 1);
-	if (!hreq)
-		return UBUS_STATUS_UNKNOWN_ERROR;
+    msgstr = blobmsg_data(tb[SCAN_DEVICE]);
 
-	sprintf(hreq->data, format, obj->name, msgstr);
-	ubus_defer_request(ctx, req, &hreq->req);
-	hreq->timeout.cb = apClient_scan_reply;
-	uloop_timeout_set(&hreq->timeout, 1000);
 
-	return 0;
+    wifi_site_survey(msgstr,NULL,0); 
+
+	blob_buf_init(&buf, 0);
+
+	c = blobmsg_open_array(&buf, "results");
+
+    for (i = 0; i < survey_count; i++)
+    {
+        d = blobmsg_open_table(&buf, NULL);
+        blobmsg_add_string(&buf, "ssid", st[i].ssid);
+        blobmsg_add_string(&buf, "bssid", st[i].bssid);
+        blobmsg_add_string(&buf, "channel", st[i].channel);
+        blobmsg_add_string(&buf, "security", st[i].security);
+        blobmsg_add_string(&buf, "siganl", st[i].siganl);
+        blobmsg_close_table(&buf, d);
+    }
+
+
+	blobmsg_close_array(&buf, c);
+
+	ubus_send_reply(ctx, req, buf.head);
+
+	return UBUS_STATUS_OK;
 }
 
 enum {
@@ -132,51 +282,65 @@ enum {
 };
 
 static const struct blobmsg_policy config_policy[__CONFIG_MAX] = {
-	[CONFIG_IFNAME] = { .name = "IFNAME", .type = BLOBMSG_TYPE_STRING },
-	[CONFIG_STANAME] = { .name = "STANAME", .type = BLOBMSG_TYPE_STRING },
-    [CONFIG_ESSID] = { .name = "ESSID", .type = BLOBMSG_TYPE_STRING },
-    [CONFIG_PASSWD] = { .name = "PASSWD", .type = BLOBMSG_TYPE_STRING },
+	[CONFIG_IFNAME] = { .name = "ifname", .type = BLOBMSG_TYPE_STRING },
+	[CONFIG_STANAME] = { .name = "staname", .type = BLOBMSG_TYPE_STRING },
+    [CONFIG_ESSID] = { .name = "essid", .type = BLOBMSG_TYPE_STRING },
+    [CONFIG_PASSWD] = { .name = "passwd", .type = BLOBMSG_TYPE_STRING },
 };
 
-static void
-apClient_handle_remove(struct ubus_context *ctx, struct ubus_subscriber *s,
-                   uint32_t id)
-{
-	fprintf(stderr, "Object %08x went away\n", id);
-}
-
-static int
-apClient_notify(struct ubus_context *ctx, struct ubus_object *obj,
-	    struct ubus_request_data *req, const char *method,
-	    struct blob_attr *msg)
-{
-#if 0
-	char *str;
-
-	str = blobmsg_format_json(msg, true);
-	fprintf(stderr, "Received notification '%s': %s\n", method, str);
-	free(str);
-#endif
-
-	return 0;
-}
 
 static int apClient_config(struct ubus_context *ctx, struct ubus_object *obj,
 		      struct ubus_request_data *req, const char *method,
 		      struct blob_attr *msg)
 {
 	struct blob_attr *tb[__CONFIG_MAX];
-	int ret;
-
+    int try_count = 0;
+    const char *ifname;
+    const char *staname;
+    const char *essid;
+    const char *passwd;
 	blobmsg_parse(config_policy, __CONFIG_MAX, tb, blob_data(msg), blob_len(msg));
-	if (!tb[CONFIG_IFNAME])
+
+    if (!tb[CONFIG_IFNAME])
 		return UBUS_STATUS_INVALID_ARGUMENT;
 
-	apClient_event.remove_cb = apClient_handle_remove;
-	apClient_event.cb = apClient_notify;
-	ret = ubus_subscribe(ctx, &apClient_event, blobmsg_get_u32(tb[CONFIG_IFNAME]));
-	fprintf(stderr, "Watching object %08x: %s\n", blobmsg_get_u32(tb[CONFIG_IFNAME]), ubus_strerror(ret));
-	return ret;
+    blob_buf_init(&buf, 0);
+    ifname = blobmsg_data(tb[CONFIG_IFNAME]);
+    staname = blobmsg_data(tb[CONFIG_STANAME]);
+    essid = blobmsg_data(tb[CONFIG_ESSID]);
+    passwd = blobmsg_data(tb[CONFIG_PASSWD]);
+    syslog(LOG_INFO, "You want to config wireless network - %s %s %s %s \n",
+        ifname, staname, essid, passwd);
+
+    while (1) {
+        struct survey_table *c;
+        wifi_site_survey(ifname, essid, 0);
+        c = wifi_find_ap(essid);
+        try_count++;
+        if (c) {
+            syslog(LOG_INFO, "Found network, trying to associate (essid: %s, bssid: %s, channel: %s, enc: %s, crypto: %s)\n",
+                essid, c->ssid, c->channel, c->security, c->crypto);
+            wifi_repeater_start(ifname, staname, c->channel, essid, passwd, c->security, c->crypto);
+        } else {
+            syslog(LOG_INFO, "No signal found to connect to\n");
+            if (try_count == 5) {
+                blobmsg_add_string(&buf, "result", "No signal"); 
+                break;
+            }
+        }
+        if (try_count == 5) {
+            blobmsg_add_string(&buf, "result", "error password"); 
+            break;
+        }
+        if (check_assoc(staname)) {
+            syslog(LOG_INFO, "%s is associated\n", staname);
+            blobmsg_add_string(&buf, "result", "success"); 
+            break;
+        }
+        sleep(1);
+    }
+    ubus_send_reply(ctx, req, buf.head);
+    return UBUS_STATUS_OK;
 }
 
 
@@ -188,10 +352,10 @@ static const struct ubus_method apClient_methods[] = {
 };
 
 static struct ubus_object_type apClient_object_type =
-	UBUS_OBJECT_TYPE("apclient", apClient_methods);
+	UBUS_OBJECT_TYPE("iwpriv", apClient_methods);
 
 static struct ubus_object apClient_object = {
-	.name = "apclient",
+	.name = "iwpriv",
 	.type = &apClient_object_type,
 	.methods = apClient_methods,
 	.n_methods = ARRAY_SIZE(apClient_methods),
@@ -204,10 +368,6 @@ static void server_main(void)
 	ret = ubus_add_object(ctx, &apClient_object);
 	if (ret)
 		fprintf(stderr, "Failed to add object: %s\n", ubus_strerror(ret));
-
-	ret = ubus_register_subscriber(ctx, &apClient_event);
-	if (ret)
-		fprintf(stderr, "Failed to add config handler: %s\n", ubus_strerror(ret));
 
 	uloop_run();
 }
@@ -232,6 +392,8 @@ int main(int argc, char **argv)
 
 	uloop_init();
 	signal(SIGPIPE, SIG_IGN);
+
+    openlog("iwpriv", 0, 0);
 
 	ctx = ubus_connect(ubus_socket);
 	if (!ctx) {
