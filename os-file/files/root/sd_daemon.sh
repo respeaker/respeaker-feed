aa90c3d23b31ca3baf64c8d2db1eab3ab8192a5b
Yihui Xiong <xiongyihui3@gmail.com>
2016-11-11 17:56:44 +0800

    bugfix

--- os-file/files/root/sd_daemon.sh
+++ os-file/files/root/sd_daemon.sh
@@ -5,13 +5,17 @@ while true
 do 
     echo "wait for mounting sd card"
     while [ ! -b /dev/mmcblk0p1 ]; do
-        sleep 1
+        sleep 3
     done
 
     if [ -f /tmp/run/mountd/mmcblk0p1/autorun ]; then
-    	echo "run autorun script on sd card" > /dev/console
-		sh /tmp/run/mountd/mmcblk0p1/autorun &
+        echo "run autorun script on sd card" > /dev/console
+        sh /tmp/run/mountd/mmcblk0p1/autorun &
     fi 
-    sleep 3
+
+    echo "wait for unmounting sd card"
+    while [ -b /dev/mmcblk0p1 ]; do
+        sleep 6
+    done
 done
 
