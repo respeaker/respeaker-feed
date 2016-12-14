7fea06659825f6d45580680f59a16aa78ac5346e
Yihui Xiong <xiongyihui3@gmail.com>
2016-12-14 17:10:57 +0800

    set volume value at startup

--- os-file/files/root/autorun.sh
+++ os-file/files/root/autorun.sh
@@ -4,6 +4,10 @@ echo "autorun"
 echo "autorun" > /dev/console
 echo "started" > /tmp/autorun
 
+amixer sset Capture 95%
+amixer sset Headphone 85%
+amixer sset Speaker 80%
+
 /root/sd_daemon.sh &
 
 
